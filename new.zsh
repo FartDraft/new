4f2a2f1e1dd6d1bcb5d157d4d10881d1c3bd8af2
”FartDraft” <fartdraft@gmail.com>
2024-01-20 23:26:47 +0300

    v1

---
+++ new.zsh
@@ -0,0 +1,19 @@
+samples=~/utils/new/samples/
+if [[ ! -e "$samples" ]]; then
+    echo "Create $samples directory"
+fi
+
+hook='nvim .'
+function new() {
+    if [[ ! -e "${samples}${1}" ]]; then
+        echo "No such sample for '${1}' language"
+        return
+    fi
+    cp -r "${samples}${1}" .
+
+    mv $1 $2
+    cd $2
+    eval $hook
+}
+
+alias new=new
